b2b2892d533603abe9529c7866dd0642bd1a7405
Laura Darlak <laura@polyporedesign.com>
2018-10-30 18:54:09 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -26,4 +26,9 @@ class FiguresController < ApplicationController
     erb :'figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
 end
