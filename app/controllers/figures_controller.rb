d7e980d60c32de7dc065ffba17caacab6a34c43d
Laura Darlak <laura@polyporedesign.com>
2018-10-30 19:01:13 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -31,6 +31,18 @@ class FiguresController < ApplicationController
     erb :'figures/edit'
   end
 
-  patch '/figures/:id'
+  patch '/figures/:id' do
+    ####### bug fix
+    if !params[:figure].keys.include?("title_ids")
+    params[:figure]["title_ids"] = []
+    end
+    #######
+
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "/owners/#{@owner.id}"
+  end
 
 end
