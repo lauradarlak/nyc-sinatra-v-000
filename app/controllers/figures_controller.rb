b5972d8997ca123d043d20da1a4556776e49e8c4
Laura Darlak <laura@polyporedesign.com>
2018-10-30 17:57:34 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,4 +3,8 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'figures/new'
   end
+
+  post '/figures' do
+    @figure = Figure.create(name: params["figure_name"])
+  end
 end
