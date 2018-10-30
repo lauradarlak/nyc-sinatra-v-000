17b12ad95d169ad0dcde96e5366e0aaa5b626e4c
Laura Darlak <laura@polyporedesign.com>
2018-10-30 18:45:35 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,7 +15,7 @@ class FiguresController < ApplicationController
     redirect "/figures/#{figure.id}"
   end
 
-  get 'figure/:id' do
+  get 'figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     erb :'figures/show'
   end
